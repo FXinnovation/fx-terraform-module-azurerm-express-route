055899fd56011b12a572a7bf98eb0bdbfb595470
Maxime Roy <maxime.roy@fxinnovation.com>
2020-03-09 11:13:50 -0400

    Removed Vnet deployment

--- examples/disabled/deploy.tf
+++ examples/disabled/deploy.tf
@@ -27,26 +27,6 @@ module "az_rg_demo" {
   tags     = local.tags
 }
 
-module "az_vnet_demo" {
-  source              = "git::https://scm.dazzlingwrench.fxinnovation.com/fxinnovation-public/terraform-module-azurerm-virtual-network.git?ref=0.1.0"
-  resource_group_name = module.az_rg_demo.name
-  location            = module.az_rg_demo.location
-  tags                = local.tags
-
-  virtual_network_name          = "fxcozca2dgenvn${random_string.this.result}"
-  virtual_network_address_space = ["10.0.0.0/16"]
-  virtual_network_dns_servers   = ["8.8.8.8", "8.8.4.4"]
-  subnets_config = {
-    gatewaysubnet = {
-      name           = "GatewaySubnet"
-      address_prefix = "10.0.0.0/24"
-    }
-  }
-
-  enable_nsg = false
-  enable_rt  = false
-}
-
 module "az_expressroute_demo" {
   source                                                  = "../../"
   enabled                                                 = false
